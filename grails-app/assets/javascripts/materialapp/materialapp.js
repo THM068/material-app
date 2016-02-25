a2b2715af21dccf5e99fbdcf989b05f04b04186d
tm1c14 <tm1c14@soton.ac.uk>
2016-02-25 14:25:39 +0000

    Init app

---
+++ grails-app/assets/javascripts/materialapp/materialapp.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /materialapp/core/materialapp.core
+//= require /materialapp/index/materialapp.index
+
+angular.module("materialapp", [
+    "materialapp.core",
+    "materialapp.index"
+]);
